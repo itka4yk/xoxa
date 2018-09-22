3593c06d8f2cc7e806efc277b51774393239f7ce
Ihor <igor.tka4yk@gmail.com>
2018-09-22 12:38:50 +0200

    add docker-compose

--- src/app.api/src/index.ts
+++ src/app.api/src/index.ts
@@ -20,4 +20,4 @@ app.use('/', express.static(path.resolve(path.join(__dirname, '..', '/wwwroot'))
 app.get('*', (_, res) => res.sendFile(path.resolve(path.join(__dirname, '..', '/wwwroot/index.html'))));
 
 
-app.listen(3001, () => console.log('Listening on port 3001'));
+app.listen(8080, () => console.log('Listening on port 8080'));
