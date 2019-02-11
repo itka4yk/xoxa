2e3741eda4a718ca0aeabedfaad39dc3d9d406b9
Ihor Tkachuk <igor.tka4yk@gmail.com>
2019-02-11 20:41:22 +0100

    cleanup

--- samples/redis/index.js
+++
@@ -1,5 +0,0 @@
-const redis = require('ioredis');
-
-const client = redis('localhost');
-console.log(client);
-client.set('KEY', 'VALUE').then(() => console.log('DONE')).catch(() => console.log("ERROR"));
